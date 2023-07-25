709b27777e8ebf90dd9d32972e73566bb53d1587
Kiselev-Dmitry <kdv150275@gmail.com>
2023-07-25 22:46:04 +0300

    Upgrade fetch_spacex_last_launch
    Add input of lunch id from command line
    Delete unuse script

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,23 +1,29 @@
 import requests
-import pathlib
-from urllib.parse import urlparse
 import os
-from dotenv import load_dotenv
-import datetime
+import argparse
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-launch_id', default='latest') # ???
+    return parser
+
 
 def fetch_spacex_last_launch(dir):
-    # Не сделал - ввод номера старта через agrparse - см. Counting clicks
-    load_dotenv()
-    spacex_id = os.environ["SPACEX_ID"]
-    spacex_url = "https://api.spacexdata.com/v5/launches/{}".format(spacex_id)
+    parser = create_parser()
+    namespace = parser.parse_args()
+    spacex_url = "https://api.spacexdata.com/v5/launches/{}".format(namespace.launch_id)
 
     if not os.path.exists(dir):
         os.makedirs(dir)
+
     response = requests.get(spacex_url)
     response.raise_for_status()
     images = response.json()["links"]["flickr"]["original"]
+#    print(images) # отладочный код
     for i, image in enumerate(images):
         response = requests.get(image)
+#        print(image)  # отладочный код
         response.raise_for_status()
         filename = '{}/hubble{}.jpeg'.format(dir,str(i))
         with open(filename, 'wb') as file:
