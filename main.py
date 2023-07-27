1c76b09832e995520f856fc9ff3c0bc9627ad908
Kiselev-Dmitry <kdv150275@gmail.com>
2023-07-28 00:06:11 +0300

    Publish text to Telegram channel

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from urllib.parse import urlparse
 import os
 from dotenv import load_dotenv
 import datetime
-
-
-
+import telegram
 
 
 if __name__ == "__main__":
     load_dotenv()
+    tg_token = os.environ["TG_TOKEN"]
+    bot = telegram.Bot(tg_token)
+    bot.send_message(chat_id="@trail_cosmos_photo", text="Greetings from the Universe")
+
 
